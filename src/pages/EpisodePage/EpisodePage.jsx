a28bd6047f5bc76d72c7711ffb18b8aefe60a97c
daniil karpov <middledkforwork@gmail.com>
2025-07-05 18:22:45 +0300

    add-EpisodePage-LocationPage-NO-pagination

--- src/pages/EpisodePage/EpisodePage.jsx
+++ src/pages/EpisodePage/EpisodePage.jsx
@@ -1,5 +1,35 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import s from './EpisodePage.module.css'
+
 export const EpisodePage = () => {
-  return (    
-      <h1 className={"pageTitle"}>EpisodePage</h1>   
+  const [episodes, setEpisode] = useState([])
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/episode').then((res) => {
+      setEpisode(res.data.results)
+    })
+  }, [])
+
+  return (
+<div className={s.pageContainer}>
+      <h1 className="pageTitle">EpisodePage</h1>
+      <div className={s.episodeList}>
+        {episodes.map((episode) => (
+          <div key={episode.id} className={s.episodeCard}>
+            <div className={s.episodeHeader}>
+              <span className={s.episodeCode}>{episode.episode}</span>
+              <span className={s.episodeName}>{episode.name}</span>
+            </div>
+            <div className={s.airDate}>
+              Дата выхода: <span className={s.bold}>{episode.air_date}</span>
+            </div>
+            <div className={s.characterListTitle}>
+              Список персонажей, которые были замечены в эпизоде: <span className={s.bold} > {episode.characters.length}</span>
+            </div>
+            
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
--- src/pages/LocationPage/LocationPage.jsx
+++ src/pages/LocationPage/LocationPage.jsx
@@ -1,5 +1,51 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import s from './LocationPage.module.css'
+
+
+
 export const LocationPage = () => {
-  return (    
-      <h1 className={"pageTitle"}>LocationPage</h1>   
+  const [locations, setLocation] = useState([])
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/location').then((res) => {
+      setLocation(res.data.results)
+    })
+  }, [])
+  return (
+    <div className={"pageContainer"}>
+      <h1 className={"pageTitle"}>LocationPage</h1>
+      {/* <div>{location.length && location[0].name}</div> */}
+      {/* <div>{Array.isArray(location) && location.length > 0 && location[0].name}</div> */}
+      {locations.length && (
+        <>
+          {
+            <>
+            
+              {locations.map((location) => {
+                return (
+                  <div key={location.id} className={s.locationItem}>
+                    <div className={s.locationName}>
+                      Название локации :<span className={s.bold}> {location.name}</span>
+                    </div>
+                    <div className={s.locationType}>
+                      Тип локации : <span className={s.bold}>{location.type}</span>
+                    </div>
+                    <div className={s.locationDimension}>
+                      Измерение: <span className={location.dimension === "unknown" ? s.unknown : s.bold}>
+                        {location.dimension}
+                      </span>
+                    </div>
+                    <div className={s.locationCount}>
+                      Количество персонажей: <span className={s.bold}>{location.residents.length}</span>
+                    </div>
+                  </div>
+                )
+              }
+              )}
+            </>
+          }
+        </>
+      )}
+    </div>
   )
 }
